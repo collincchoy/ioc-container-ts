e8255a7416a1035ad92b6246048ea75045a83d33
Collin Choy <collin@choy.me>
2023-07-20 00:15:51 -0400

    Scaffold together container/service registry.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-function say(message: string): void {
-  console.log(message);
+import { Container } from "./container";
+
+class Logger {
+  log(message: string) {
+    console.log(message);
+  }
 }
 
-function main() {
-  say("hello, Mr. Alien");
+class Service {
+  private logger: Logger;
+
+  constructor(logger: Logger) {
+    this.logger = logger;
+  }
+
+  doSomething() {
+    this.logger.log("Doing something...");
+  }
 }
 
-main();
+const container = new Container();
+container.register<Logger>(Logger, () => new Logger());
+container.register<Service>(
+  Service,
+  () => new Service(container.resolve<Logger>(Logger))
+);
+
+const service = container.resolve<Service>(Service);
+service.doSomething();
