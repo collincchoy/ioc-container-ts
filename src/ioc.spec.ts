b135303bf281d77480ac5ab7d7c27a4084af1d89
Collin Choy <collin@choy.me>
2023-07-20 18:16:44 -0400

    Add test coverage for singleton option.

--- src/ioc.spec.ts
+++ src/ioc.spec.ts
@@ -8,13 +8,23 @@ describe("container", () => {
     expect(container).toBeDefined();
   });
 
-  it("can register and resolve services", () => {
+  it("can register and resolve unique(non-singleton) services", () => {
     const container = Container.init();
     class Service {}
 
     container.register(Service, () => new Service());
 
     expect(container.resolve(Service)).toBeInstanceOf(Service);
+    expect(container.resolve(Service)).not.toBe(container.resolve(Service));
+  });
+
+  it("can register and resolve singleton services", () => {
+    const container = Container.init();
+    class Service {}
+
+    container.register(Service, () => new Service(), { singleton: true });
+
+    expect(container.resolve(Service)).toBe(container.resolve(Service));
   });
 
   // vitest doesn't support new ES/TS5 decorators https://github.com/vitest-dev/vitest/issues/3140
