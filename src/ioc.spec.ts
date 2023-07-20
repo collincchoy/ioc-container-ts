c03d357a15f00c0ef46be7b64e5305e645ea3f2e
Collin Choy <collin@choy.me>
2023-07-20 18:09:21 -0400

    remove dead code.

--- src/ioc.spec.ts
+++ src/ioc.spec.ts
@@ -1,13 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { Container, Injectable } from "./ioc";
-import { beforeEach } from "node:test";
 
 describe("container", () => {
-  // let container;
-  // beforeEach(() => {
-  //   container = Container.init();
-  // });
-
   it("initializes correctly", () => {
     const container = new Container();
 
